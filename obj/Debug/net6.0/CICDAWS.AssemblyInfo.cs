60a4fd811ad96998164a436ed9eaa95fd3548a04
nilton <ngcquimica@gmail.com>
2024-02-11 15:49:25 -0300

    removing openai

--- obj/Debug/net6.0/CICDAWS.AssemblyInfo.cs
+++ obj/Debug/net6.0/CICDAWS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CICDAWS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c3ca939beb9b23db4ddc7edf7665d14734d9c7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ac011ab63879c58caf6fbb930087f703f01d9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CICDAWS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CICDAWS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
